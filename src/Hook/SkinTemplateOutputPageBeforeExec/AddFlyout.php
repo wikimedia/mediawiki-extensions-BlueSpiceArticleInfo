5bdff06130b34ca76a1ac0edae4093217ce44d76
Dejan Savuljesku <d.savuljesku@gmail.com>
2018-07-05 15:29:18 +0200

    Add conditions to showing Flyout button
    Change-Id: Ib6b3651021387edba1611aebcd5ab8d094d74e82

--- src/Hook/SkinTemplateOutputPageBeforeExec/AddFlyout.php
+++ src/Hook/SkinTemplateOutputPageBeforeExec/AddFlyout.php
@@ -8,6 +8,17 @@ use BlueSpice\SkinData;
 use BlueSpice\ArticleInfo\Panel\Flyout;
 
 class AddFlyout extends SkinTemplateOutputPageBeforeExec {
+	protected function skipProcessing() {
+		$title = $this->skin->getSkin()->getTitle();
+		if( !$title instanceof \Title || !$title->exists() || !$title->isContentPage() ) {
+			return true;
+		}
+		if( $title->userCan( 'read' ) === false ) {
+			return true;
+		}
+		return false;
+	}
+
 	protected function doProcess() {
 
 		$this->mergeSkinDataArray(
