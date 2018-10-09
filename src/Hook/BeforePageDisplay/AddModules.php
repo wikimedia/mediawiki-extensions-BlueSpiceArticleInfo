fa987186ca1d9e5d072f6fcd5da1008495ec5d1f
pwirth <wirth@hallowelt.biz>
2018-10-09 20:22:16 +0200

    Seperate styles from modules
    Change-Id: Id2863e24bb1f5eda9f765e05a4ebf9752f883f7c

--- src/Hook/BeforePageDisplay/AddModules.php
+++ src/Hook/BeforePageDisplay/AddModules.php
@@ -8,6 +8,7 @@ class AddModules extends BeforePageDisplay {
 
 	protected function doProcess() {
 		$this->out->addModules( 'ext.bluespice.articleinfo.general' );
+		$this->out->addModuleStyles( 'ext.bluespice.articleinfo.flyout.styles' );
 		return true;
 	}
 
