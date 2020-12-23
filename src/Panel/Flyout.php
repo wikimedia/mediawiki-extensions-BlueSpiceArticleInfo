c855f507c62d42daeccab7d5f6e95eb1d53dd23b
dvogel <daniel.vogel@hallowelt.com>
2020-12-23 10:49:43 +0100

    Modify flyout pills and use api store for categorylinks and templatelinks
    [3.1.x]
    
    ERM22605
    
    Change-Id: I1778a40bf893f8f190eee4f473789680ca308ab4

--- src/Panel/Flyout.php
+++ src/Panel/Flyout.php
@@ -2,11 +2,11 @@
 
 namespace BlueSpice\ArticleInfo\Panel;
 
+use BlueSpice\Calumma\IFlyout;
+use BlueSpice\Calumma\Panel\BasePanel;
+use BlueSpice\Services;
 use Message;
 use QuickTemplate;
-use BlueSpice\Services;
-use BlueSpice\Calumma\Panel\BasePanel;
-use BlueSpice\Calumma\IFlyout;
 
 class Flyout extends BasePanel implements IFlyout {
 	/**
@@ -96,16 +96,6 @@ class Flyout extends BasePanel implements IFlyout {
 		$lastEditor = $this->getLastEditedUser();
 		if ( $lastEditor ) {
 			$data['last-edited-user'] = $lastEditor;
-		}
-
-		$categoryLinks = $this->getCategoryLinks();
-		if ( $categoryLinks ) {
-			$data['category-links'] = $categoryLinks;
-		}
-
-		$templateLinks = $this->getTemplateLinks();
-		if ( $templateLinks ) {
-			$data['template-links'] = $templateLinks;
 		}
 
 		$data['has-subpages'] =
@@ -236,56 +226,5 @@ class Flyout extends BasePanel implements IFlyout {
 			'userText' => $userHelper->getDisplayName(),
 			'url' => $lastEditor->getUserPage()->getFullURL()
 		] );
-	}
-
-	/**
-	 * Gets all categories of the page
-	 * @return string
-	 */
-	protected function getCategoryLinks() {
-		$allPageCategoryLinks = $this->skintemplate->getSkin()->getOutput()->getCategoryLinks();
-
-		$pageCategoryLinks = [];
-		if ( isset( $allPageCategoryLinks['normal'] ) ) {
-			$pageCategoryLinks = $allPageCategoryLinks['normal'];
-		}
-
-		if ( $this->skintemplate->getSkin()->getUser()->getBoolOption( 'showhiddencats' ) ) {
-			if ( isset( $allPageCategoryLinks['hidden'] ) ) {
-				$pageCategoryLinks = array_merge(
-					$pageCategoryLinks,
-					$allPageCategoryLinks['hidden']
-				);
-			}
-		}
-
-		// This is just so that the ExtJS store can handle it more easily
-		$keyedCategoryLinks = [];
-		foreach ( $pageCategoryLinks as $categoryLink ) {
-			$keyedCategoryLinks[] = [
-				'category_anchor' => $categoryLink,
-				'class' => 'pill'
-			];
-		}
-
-		return \FormatJson::encode( $keyedCategoryLinks );
-	}
-
-	/**
-	 *
-	 * @return string
-	 */
-	protected function getTemplateLinks() {
-		$linkRenderer = Services::getInstance()->getLinkRenderer();
-		$templateTitles = $this->title->getTemplateLinksFrom();
-
-		$templateLinks = [];
-		foreach ( $templateTitles as $title ) {
-			$templateLinks[] = [
-				'template_anchor' => $linkRenderer->makeLink( $title, $title->getText() )
-			];
-		}
-
-		return \FormatJson::encode( $templateLinks );
 	}
 }
