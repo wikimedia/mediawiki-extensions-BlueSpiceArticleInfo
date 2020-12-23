44ea19aaadd3fdcc883a6ef5145da200184598ad
dvogel <daniel.vogel@hallowelt.com>
2020-12-23 10:49:43 +0100

    Modify flyout pills and use api store for categorylinks and templatelinks
    [3.1.x]
    
    ERM22605
    
    Change-Id: I1778a40bf893f8f190eee4f473789680ca308ab4

--- resources/BS.ArticleInfo/panel/LinkList.js
+++ resources/BS.ArticleInfo/panel/LinkList.js
@@ -4,41 +4,52 @@ Ext.define( 'BS.ArticleInfo.panel.LinkList', {
 	title: '',
 	storeField: '',
 	emptyText: '',
-
+	apiAction: null,
 	linkList: {},
 	listType: 'ul',
 	initComponent: function () {
-		this.store = new Ext.data.Store( {
-			fields: [ this.storeField ],
-			data: this.linkList,
-			autoLoad: true
-		} );
-
-		this.template = new Ext.XTemplate(
-			"<ul class='bs-articleinfo-flyout-linklist'>",
-			"<tpl for='.'>",
-				"<li>{" + this.storeField + "}</li>",
-			"</tpl></ul>"
-		);
-
-		if( this.listType === 'pills' ) {
-			this.template = new Ext.XTemplate(
-				"<div class='bs-articleinfo-flyout-linklist'>",
-				"<tpl for='.'>",
-					"<span class='pill'>{" + this.storeField + "}</span>",
-				"</tpl></div>"
-			);
+		if ( this.apiAction !== null ) {
+			this.store = Ext.create( 'BS.store.BSApi', {
+				apiAction: this.apiAction,
+				fields: [ this.storeField ],
+				filters: [ {
+					"type": "numeric",
+					"operator": "eq",
+					"property": "page_id",
+					"value": mw.config.get( 'wgArticleId' )
+				} ],
+				limit: -1,
+				autoLoad: false
+			} );
+		} else {
+			this.store = new Ext.data.Store( {
+				fields: [ this.storeField ],
+				data: this.linkList,
+				autoLoad: false
+			} );
 		}
 
-		this.items = [
-			new Ext.DataView( {
-				store: this.store,
-				tpl: this.template,
-				itemSelector: '.storeitem', //No selection needed here
-				emptyText: this.emptyText
-			} )
-		];
+		this.cls = 'bs-articleinfo-flyout-linklist';
+		if ( this.listType === 'pills' ) {
+			this.cls += ' pills';
+		}
+
+
+		this.store.on( 'load', this.onStoreLoad, this );
+		this.store.load();
 
 		this.callParent( arguments );
+	},
+
+	onStoreLoad: function ( store, records, successful, eOpts ) {
+		var links = [];
+		for ( var i = 0; i < records.length; i++ ) {
+			var record = records[i];
+			links.push( record.get( this.storeField ) );
+		}
+
+		var html = '<div class="' + this.cls + '">' + links.join( '' ) + '</div>';
+
+		this.update( html );
 	}
 });
